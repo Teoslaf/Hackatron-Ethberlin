efcd007f4162e0d3710c3003d367eed607078d28
Teodor Taneski Feldschau <50484090+Teoslaf@users.noreply.github.com>
2024-05-25 23:02:49 +0200

    ZKP

---
+++ verify.js
@@ -0,0 +1,17 @@
+const snarkjs = require('snarkjs');
+const fs = require('fs');
+
+const input = { in: 10 };
+
+(async () => {
+    const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+        input,
+        "build/poseidon_hasher_js/poseidon_hasher.wasm",
+        "circuit_0000.zkey"
+    );
+    console.log("Public signals:", publicSignals);
+    console.log("Proof:", proof);
+
+    fs.writeFileSync("proof.json", JSON.stringify(proof));
+    fs.writeFileSync("public.json", JSON.stringify(publicSignals));
+})();
